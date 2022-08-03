3d7eeedd951ec983c1b2343feb42960aba8f5190
Davidliduena <Davidliduena13@gmail.com>
2022-08-03 17:32:54 -0500

    cambios

--- abmclientes/index.php
+++ abmclientes/index.php
@@ -99,8 +99,6 @@ if(isset($_GET["do"]) && $_GET["do"] == "eliminar"){
     <title>ABM Clientes</title>
    <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<link rel="stylesheet" href="css/fontawesome6/css/all.min.css">
-   <link rel="stylesheet" href="css/fontawesome6/css/fontawesome.min.css">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 <body>
